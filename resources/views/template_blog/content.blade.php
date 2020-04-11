5b509efd1075441feda8a82365a0ca4a5a4f5d00
ghaly rizqi <rizqirr@gmail.com>
2020-04-11 22:50:07 +0700

    isi_post

---
+++ resources/views/template_blog/content.blade.php
@@ -0,0 +1,13 @@
+    <!-- SECTION -->
+    @include('template_blog.head')
+	<div class="section">
+		<!-- container-->
+		<div class="container">
+            			<!-- row -->
+			<div id="hot-post" class="row hot-post">
+			    <!-- post -->
+                @yield('isi')
+                @include('template_blog.widget')
+        </div>
+    </div>
+    @include('template_blog.footer')
